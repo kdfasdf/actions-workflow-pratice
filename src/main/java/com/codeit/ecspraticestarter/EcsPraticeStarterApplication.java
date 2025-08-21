a954a9f0a6698bb973b74f559a9003120228f951
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-08-21 15:43:34 +0900

    first commit

---
+++ src/main/java/com/codeit/ecspraticestarter/EcsPraticeStarterApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.ecspraticestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsPraticeStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcsPraticeStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/ecspraticestarter/EcsPraticeStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.ecspraticestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsPraticeStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
